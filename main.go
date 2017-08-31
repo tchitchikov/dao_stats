6236b1485a76439d0271964c7848f74ae53c1296
tchitchikov <jinterlan5@gmail.com>
2017-08-30 23:07:32 -0400

    added some stats package for go with chinese labels

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"simpleStats"
+)
+
+func main() {
+	价值表 := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 17.0}
+	fmt.Println(simpleStats.S_平均(价值表))
+	fmt.Println(simpleStats.S_方差(价值表))
+}
---
+++ src/simpleStats/simpleStatBase.go
@@ -0,0 +1,24 @@
+package simpleStats
+
+import (
+	"math"
+)
+
+func S_和(值 []float64) (返回值 float64) {
+	for _, x := range 值 {
+		返回值 = 返回值 + x
+	}
+	return 返回值
+}
+
+func S_平均(值 []float64) (返回值 float64) {
+	return S_和(值) / float64(len(值))
+}
+
+func S_方差(值 []float64) (返回值 float64) {
+	平均 := S_平均(值)
+	for _, x := range 值 {
+		返回值 = 返回值 + math.Pow((x-平均), 2)
+	}
+	return 返回值 / float64(len(值)-1)
+}
