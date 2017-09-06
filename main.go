755827383483f074873561176a441ea819814571
tchitchikov <jinterlan5@gmail.com>
2017-09-06 08:33:21 -0400

    getting the standard deviation of the google closing price sample

--- main.go
+++ main.go
@@ -12,5 +12,10 @@ func main() {
 	fmt.Println(simpleStats.S_平均(价值表))
 	fmt.Println(simpleStats.S_方差(价值表))
 	fmt.Println(simpleStats.S_标准偏差(价值表))
-	fmt.Println(数据.Google数据("GOOG", "2017-7-1", "2017-7-31"))
+	Google数据 := 数据.Google数据("GOOG", "2017-7-1", "2017-7-31")
+	var GoogleClose数据 []float64
+	for _, 行 := range Google数据 {
+		GoogleClose数据 = append(GoogleClose数据, 行.Close)
+	}
+	fmt.Println(simpleStats.S_标准偏差(GoogleClose数据))
 }
